eefdd088f18478d36b4f50c63c9a1676b4f724a5
PerrierBottle <91777437+PerrierBouteille@users.noreply.github.com>
2022-05-13 17:46:32 +0200

    Add files via upload

---
+++ Install.bat
@@ -0,0 +1,5 @@
+@echo off
+
+py -m pip install pygame
+py -m pip install pyyaml
+py -m pip install pyoperator
---
+++ Install.sh
@@ -0,0 +1,3 @@
+py -m pip install pygame
+py -m pip install pyyaml
+py -m pip install pyoperator
